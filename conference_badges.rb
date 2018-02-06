431ade9e2ed5595125a6f8788f5507e92053e562
Chely Ho <adept_inept@yahoo.com>
2018-02-06 22:58:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,9 +18,9 @@ def assign_rooms(speakers)
 end
 
 def printer(speakers)
-  batch_badge_creator(speakers).each do |speaker|
-    print speaker
+  batch_badge_creator(speakers).each do |badge|
+    print badge
   end
-    
-  
+
+
 end
